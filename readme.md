e1976b1a7780944c9b91018d7dcd743a52650565
Alex Gisby <alex@solution10.com>
2014-11-06 18:00:50 +0000

    Initial commit with sketch of how the trait will work

---
+++ readme.md
@@ -0,0 +1,55 @@
+# Solution10\Managed-Instance
+
+Allows classes to manage instances of themselves either as singletons or "lazy DI" containers.
+
+[![Build Status](https://travis-ci.org/solution10/managed-instance.svg?branch=master)](https://travis-ci.org/solution10/managed-instance)
+[![Coverage Status](https://coveralls.io/repos/solution10/managed-instance/badge.png)](https://coveralls.io/r/solution10/managed-instance)
+
+[![Latest Stable Version](https://poser.pugx.org/solution10/managed-instance/v/stable.svg)](https://packagist.org/packages/solution10/managed-instance)
+[![Total Downloads](https://poser.pugx.org/solution10/managed-instance/downloads.svg)](https://packagist.org/packages/solution10/managed-instance)
+[![License](https://poser.pugx.org/solution10/managed-instance/license.svg)](https://packagist.org/packages/solution10/managed-instance)
+
+
+## Installation
+
+Installation is via composer, in the usual manner:
+
+```json
+{
+    "require" {
+        "solution10/managed-instance": "1.*"
+    }
+}
+```
+
+## Documentation
+
+### Userguide
+
+[Check out the Wiki](https://github.com/solution10/managed-instance/wiki)
+
+(or the /docs folder in the repo)
+
+### API Docs
+
+From a checkout of this project, run:
+
+    $ make
+
+This will create an api/ folder for you to peruse.
+
+## PHP Requirements
+
+- PHP >= 5.4
+
+## Author
+
+Alex Gisby: [GitHub](http://github.com/alexgisby), [Twitter](http://twitter.com/alexgisby)
+
+## License
+
+[MIT](http://github.com/solution10/managed-instance/tree/master/LICENSE.md)
+
+## Contributing
+
+[Contributors Notes](http://github.com/solution10/managed-instance/tree/master/CONTRIBUTING.md)
---
+++ src/ManagedInstance.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Solution10\ManagedInstance;
+
+/**
+ * Trait ManagedInstance
+ *
+ * Adding this trait will allow your classes to manage instances of themselves, without
+ * dumping everything globally or in DI containers.
+ *
+ * @package Solution10\ManagedInstance
+ */
+trait ManagedInstance
+{
+    /**
+     * @var     ManagedInstance[]
+     */
+    protected static $instances = [];
+
+    /**
+     * @var     string
+     */
+    protected $instanceName = 'default';
+
+    /**
+     * Returns an instance of this object, by a given name (null for default). If one doesn't exist, it will
+     * create one for you using instanceFactory()
+     *
+     * @param   string|null  Either the instance by a given name, or the default one for singletons.
+     * @return  self
+     */
+    public static function instance($name = null)
+    {
+        if ($name === null) {
+            $name = 'default';
+        }
+
+        if (!array_key_exists($name, self::$instances)) {
+            self::$instances[$name] = self::instanceFactory($name);
+            self::$instances[$name]->instanceName($name);
+        }
+
+        return self::$instances[$name];
+    }
+
+    /**
+     * Returns a new instance of the class this trait is attached to.
+     * This is your factory, or override point if you do something weird
+     * just after construction.
+     *
+     * @param   string  $name   The name that this instance is being saved under.
+     * @return  self
+     */
+    protected static function instanceFactory($name)
+    {
+        $class = get_called_class();
+        return new $class();
+    }
+
+    /**
+     * Returns all instances associated with this object in a name => instance map.
+     *
+     * @return  array
+     */
+    public static function instances()
+    {
+        return self::$instances;
+    }
+
+    /**
+     * Get/Set the name of this instance
+     *
+     * @param   string|null     $name   String to set, null to get
+     * @return  $this|string
+     */
+    public function instanceName($name = null)
+    {
+        if ($name === null) {
+            return $this->instanceName;
+        }
+        $this->instanceName = $name;
+        return $this;
+    }
+
+    /**
+     * This function manually registers an instance of this object with the management routines.
+     * Helpful if you have hand-cranked this object by calling construct explicitly.
+     *
+     * @param   string  $name   Name to use, null for 'default'
+     * @return  $this
+     */
+    public function registerInstance($name = null)
+    {
+        if ($name === null) {
+            $name = 'default';
+        }
+        self::$instances[$name] = $this;
+        $this->instanceName($name);
+        return $this;
+    }
+
+    /**
+     * Unregisters an instance with the manager.
+     *
+     * @return  $this
+     */
+    public function unregisterInstance()
+    {
+        if (array_key_exists($this->instanceName, self::$instances)) {
+            unset(self::$instances[$this->instanceName]);
+        }
+        return $this;
+    }
+}
